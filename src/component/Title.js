c1e0fd6aae7a17030b21d5cd9f3f8203e374bb45
vivek0042 <vivekjain.0042@gmail.com>
2023-03-03 11:26:23 +0530

    task-2

--- src/component/Title.js
+++ src/component/Title.js
@@ -12,7 +12,11 @@ import Home from "../pages/Home";
 // this is for task 3:
 
 const Title = props => {
-   return <h1>{props.title}</h1>
+   const {name,title,Desctription} = props
+   return <div>
+            <h1>{title}</h1>
+            <h2>{Desctription}</h2>
+           </div> 
 
 }
 export default Title
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,18 +4,18 @@ import Title from '../component/Title'
 
 class Home extends Component{
 // task 3:-making state for the title.js
-    // constructor(){
-    //     super()
-    //     this.State={
-    //       title:"Greetings from Tatvasoft",
-    //       Description:"please complete your task"
-    //     }}
+    constructor(){
+        super()
+        this.State={
+          title:"Greetings from Tatvasoft",
+          Description:"please complete your task"
+        }}
     render(){
         return (
         <div>
           <h1>this is home page</h1>
-          {/* <h1>{this.State.title}</h1>    //task 3:- */}
-          {/* <Title/> */}
+          {/* <h1>{this.State.title}</h1>     */}
+          <Title name="virat" title={this.State.title} Description={this.State.Description}/>
           {/* <Title name="srujan"/>    //this is for task2:-
           <Title name="hardik"/> */}
         </div>
